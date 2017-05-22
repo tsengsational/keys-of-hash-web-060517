252479037834cf7aa9c2554e741404728e440413
Jason Tseng <jason.f.tseng@gmail.com>
2017-05-22 15:50:09 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+    self.each {|key, item|
+      array << key if arguments.include?(item)
+     }
+     array
   end
 end
